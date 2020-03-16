bb3e491b771fe5a7b1b346975e7f02bd1078fb6d
Vadym Kostiuk <vadim.dk81@gmail.com>
2020-03-16 22:30:03 +0200

    feat: add action for nav

---
+++ script.js
@@ -0,0 +1,6 @@
+const MENU = document.getElementById('menu');
+
+MENU.addEventListener('click', (event) => {
+    MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'));
+    event.target.classList.add('active');
+});
